11e16ad366698e3be5b2a39eb74c9efca10fe08c
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-26 01:01:57 +0200

    Udacity Sensor Fusion: new function to do O(1) raycast of a car
    Added a new checkRayHit function that checks if a ray hits (not only a
    point). This requires some helper function that are also contained in this
    commit.

--- SFND_Lidar_Obstacle_Detection/src/render/render.h
+++ SFND_Lidar_Obstacle_Detection/src/render/render.h
@@ -8,6 +8,7 @@
 #include "box.h"
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 struct Color
@@ -59,6 +60,8 @@ struct Car
 
 	// units in meters
   	Vect3 position, dimensions;
+  	Vect3 box1min, box1max;
+  	Vect3 box2min, box2max;
   	
   	std::string name;
   	Color color;
@@ -66,6 +69,10 @@ struct Car
   	Car(Vect3 setPosition, Vect3 setDimensions, Color setColor, std::string setName)
     	: position(setPosition),
     	  dimensions(setDimensions),
+          box1min ( position + Vect3( -dimensions.x / 2.0, -dimensions.y / 2.0, 0.0) ),
+          box1max ( position + Vect3(  dimensions.x / 2.0,  dimensions.y / 2.0, dimensions.z * 2.0 / 3.0) ),
+          box2min ( position + Vect3( -dimensions.x / 4.0, -dimensions.y / 2.0, dimensions.z  * 2.0 / 3.0) ),
+          box2max ( position + Vect3(  dimensions.x / 4.0,  dimensions.y / 2.0, dimensions.z) ),
     	  color(setColor),
     	  name(setName)
   	{
@@ -85,6 +92,85 @@ struct Car
 			   (inbetween(point.x,position.x,dimensions.x/4)&&inbetween(point.y,position.y,dimensions.y/2)&&inbetween(point.z,position.z+dimensions.z*5/6,dimensions.z/6));
 
 	}
+	// from chaosTechnician: https://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
+    // Helper function for Line/AABB test.  Tests collision on a single dimension
+    // Param:    Start of line, Direction/length of line,
+    //           Min value of AABB on plane, Max value of AABB on plane
+    //           Enter and Exit "timestamps" of intersection (OUT)
+    // Return:   True if there is overlap between Line and AABB, False otherwise
+    // Note:     Enter and Exit are used for calculations and are only updated in case of intersection
+    bool Line_AABB_1d(double start, double dir, double min, double max, double& enter, double& exit)
+    {
+        //If the line segment is more of a point, just check if it's within the segment
+        if(fabs(dir) < 1.0E-8)
+            return (start >= min && start <= max);
+
+        //Find if the lines overlap
+        double   ooDir = 1.0 / dir;
+        double   t0 = (min - start) * ooDir;
+        double   t1 = (max - start) * ooDir;
+
+        //Make sure t0 is the "first" of the intersections
+        if(t0 > t1)
+            std::swap(t0, t1);
+
+        //Check if intervals are disjoint
+        if(t0 > exit || t1 < enter)
+        {
+            return false;
+        }
+
+        //Reduce interval based on intersection
+        if(t0 > enter)
+            enter = t0;
+        if(t1 < exit)
+            exit = t1;
+
+        return true;
+    }
+
+	// from chaosTechnician: https://gamedev.stackexchange.com/questions/18436/most-efficient-aabb-vs-ray-collision-algorithms
+    // Check collision between a line segment and an AABB
+    // Param:    Start point of line segement, End point of line segment,
+    //           One corner of AABB, opposite corner of AABB,
+    //           Location where line hits the AABB (OUT)
+    // Return:   True if a collision occurs, False otherwise
+    // Note:     If no collision occurs, OUT param is not reassigned and is not considered useable
+    bool Line_AABB(const Vect3& s, const Vect3& dir, const Vect3& min, const Vect3& max, Vect3& hitPoint, double minDist, double &distance)
+    {
+        double enter = minDist;
+        double exit = distance;
+
+        //Check each dimension of Line/AABB for intersection
+        if(!Line_AABB_1d(s.x, dir.x, min.x, max.x, enter, exit))
+            return false;
+        if(!Line_AABB_1d(s.y, dir.y, min.y, max.y, enter, exit))
+            return false;
+        if(!Line_AABB_1d(s.z, dir.z, min.z, max.z, enter, exit))
+            return false;
+
+        //If there is intersection on all dimensions, report that point
+        distance = enter;
+        hitPoint = s + distance * dir;
+
+        return true;
+    }
+
+    /**
+     * bCheck if ray from origin + a*direction hits the car.
+     * @params origin      Starting point of the ray
+     *         direction   Direction of the ray - maximal distance * direction must correspond to the actual final distance
+     *         hitPoint    If the car is hit, the hitpoint will be set to the closest point
+     *         minDist     Minimal distance that needs to be checked
+     *         distance    Maximal distance that needs to be checked; if the vehicle is hit, it will contain the distance to the vehicle
+     */ 
+	bool checkRayCollision(const Vect3 &origin, const Vect3 &direction, Vect3 &hitPoint, double minDist, double &distance)
+    {
+        bool hit1 = Line_AABB( origin, direction, box1min, box1max, hitPoint, minDist, distance );
+        bool hit2 = Line_AABB( origin, direction, box2min, box2max, hitPoint, minDist, distance );
+        
+        return ( hit1 || hit2 );
+    }
 };
 
 void renderHighway(pcl::visualization::PCLVisualizer::Ptr& viewer);

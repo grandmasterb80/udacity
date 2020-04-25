ed1da18b033f6b909e974744d28e4f45d60aec00
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-26 01:09:47 +0200

    Udacity Sensor Fusion: added solution for the Cluster quiz: kdtree

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -1,6 +1,7 @@
 /* \author Aaron Brown */
 // Quiz on implementing kd tree
 
+#include <cassert>
 #include "../../render/render.h"
 
 
@@ -15,6 +16,13 @@ struct Node
 	Node(std::vector<float> arr, int setId)
 	:	point(arr), id(setId), left(NULL), right(NULL)
 	{}
+
+	~Node()
+    {
+         // will recursively delete all leaves
+        if( left != nullptr ) delete left;
+        if( right != nullptr ) delete right;
+    }
 };
 
 struct KdTree
@@ -25,11 +33,58 @@ struct KdTree
 	: root(NULL)
 	{}
 
+	~KdTree()
+    {
+        if( root != nullptr ) delete root; // will recursively delete all leaves
+    }
+
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly within the root 
+        uint32_t numAxes = point.size();
+        Node* newNode = new Node( point, id );
+        
+        if( root == nullptr )
+        {
+            root = newNode;
+        }
+        else
+        {
+            Node* currentNode = root;
+            bool axis = 0;  // 0 = X, 1 = Y, 2 = ...
+            bool inserted = false;
+            do {
+                assert( currentNode->point.size() == numAxes );
 
+                if( point[ axis ] < currentNode->point[ axis ] )
+                {
+                    if( currentNode->left == nullptr )
+                    {
+                        currentNode->left = newNode;
+                        inserted = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->left;
+                    }
+                }
+                else
+                {
+                    if( currentNode->right == nullptr )
+                    {
+                        currentNode->right = newNode;
+                        inserted = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->right;
+                    }
+                }
+
+                axis = ( axis + 1 ) % numAxes;
+            } while( !inserted );
+        }
 	}
 
 	// return a list of point ids in the tree that are within distance of target

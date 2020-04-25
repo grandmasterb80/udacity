f9670b484e4b97f37cc7500cadd5afc3f0dbdd6d
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-25 12:42:05 +0200

    Udacity: Sensor Fusion course fixed typo

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -28,7 +28,7 @@ struct KdTree
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly within the root 
 
 	}
 

47982cda54654f4c3af58959923fb418dcba3c79
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-26 03:57:31 +0200

    Udacity Sensor Fusion: added simplified / less performant function for the search in KD tree

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -179,10 +179,39 @@ struct KdTree
             window_top_left[ axis ] = oldBoundLeft;
         }
     }
+
+    /*
+    void recSearch2( Node *node, std::vector<int> &ids, int axis, std::vector<float> &target, double distanceTol )
+    {
+        if( node == nullptr ) return;
+        assert( node->point.size() == target.size() );
+
+        double dd = 0.0;
+        for( int i = 0; i < node->point.size(); i++ )
+        {
+            double d = node->point[ i ] - target[ i ];
+            dd += d * d;
+        } 
+        if( dd <= distanceTol * distanceTol )
+        {
+            ids.push_back( node->id );
+        }
+
+        int nextAxis = ( axis + 1 ) % node->point.size();
+        if( node->point[ axis ] >= target[ axis ] - distanceTol )
+            recSearch2( node->left, ids, nextAxis, target, distanceTol );
+        if( node->point[ axis ] <= target[ axis ] + distanceTol )
+            recSearch2( node->right, ids, nextAxis, target, distanceTol );
+    }
+    */
+
+	
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+//        recSearch2( root, ids, 0, target, distanceTol );
+
         int numMatches = 0;
         std::vector<float> box_top_left, box_bottom_right;
         std::vector<float> window_top_left, window_bottom_right;

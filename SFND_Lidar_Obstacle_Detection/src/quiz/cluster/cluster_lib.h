33df8b500c2f34554f419e71f3d4db141888b7c1
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-26 21:59:11 +0200

    Udacity Sensor Fusion: moved euclidean clustering function into cpp file
    The function was left in the header file due to expected definition of
    template. Turned out that there is no need for adding a template
    definition, therefore it is now in a CPP file.

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster_lib.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster_lib.h
@@ -10,41 +10,6 @@
 #include <string>
 #include "kdtree.h"
 
-std::vector< std::vector < int > > euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
-{
-	// TODO: Fill out this function to return list of indices for each cluster
-    std::vector< std::vector < int > > clusters;
-// --------------------- DANIEL SOLUTION START ---------------------
-    std::set< int > visitedPoints;
-    for( int currentPoint = 0; currentPoint != points.size(); currentPoint++ )
-    {
-        std::set < int > newClusterSet;
-        std::vector< int > pointsToCheck;
-        if( visitedPoints.count( currentPoint ) > 0 ) continue;
-        pointsToCheck.push_back( currentPoint );
-        do
-        {
-            int pIndex = pointsToCheck.back();
-            pointsToCheck.pop_back();
-            if( visitedPoints.count( pIndex ) > 0 ) continue;
-            visitedPoints.insert ( pIndex );
-            newClusterSet.insert( pIndex );
-            std::vector< int > pointsToCheckNew = tree->search( points[ pIndex ], distanceTol );
-            pointsToCheck.insert( pointsToCheck.end(), pointsToCheckNew.begin(), pointsToCheckNew.end() );
-        } while( !pointsToCheck.empty() );
-
-        std::vector < int > newCluster( newClusterSet.begin(), newClusterSet.end() );
-        clusters.push_back( newCluster );
-    }
-    std::cout << "euclideanCluster: found " << clusters.size() << " clusters with size {";
-    for( std::vector<int> c : clusters )
-    {
-        std::cout << c.size() << ", ";
-    }
-    std::cout << "}" << std::endl;
-// --------------------- DANIEL SOLUTION END ---------------------
-
-	return clusters;
-}
+std::vector< std::vector < int > > euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol);
 
 #endif // CLUSTER_LIB_H

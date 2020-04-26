dfd593463bd5753a2275f438ea509545e305ebad
Daniel Baudisch <daniel.baudisch@web.de>
2020-04-26 03:59:42 +0200

    Udacity Sensor Fusion: completed euclideanCluster function to cluster points

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster.cpp
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/cluster.cpp
@@ -76,11 +76,36 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 std::vector< std::vector < int > > euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 	// TODO: Fill out this function to return list of indices for each cluster
+    std::vector< std::vector < int > > clusters;
+    std::set< int > visitedPoints;
+    for( int currentPoint = 0; currentPoint != points.size(); currentPoint++ )
+    {
+        std::set < int > newClusterSet;
+        std::vector< int > pointsToCheck;
+        if( visitedPoints.count( currentPoint ) > 0 ) continue;
+        pointsToCheck.push_back( currentPoint );
+        do
+        {
+            int pIndex = pointsToCheck.back();
+            pointsToCheck.pop_back();
+            if( visitedPoints.count( pIndex ) > 0 ) continue;
+            visitedPoints.insert ( pIndex );
+            newClusterSet.insert( pIndex );
+            std::vector< int > pointsToCheckNew = tree->search( points[ pIndex ], distanceTol );
+            pointsToCheck.insert( pointsToCheck.end(), pointsToCheckNew.begin(), pointsToCheckNew.end() );
+        } while( !pointsToCheck.empty() );
 
-	std::vector<std::vector<int>> clusters;
- 
+        std::vector < int > newCluster( newClusterSet.begin(), newClusterSet.end() );
+        clusters.push_back( newCluster );
+    }
+    std::cout << "euclideanCluster: found " << clusters.size() << " clusters with size {";
+    for( std::vector<int> c : clusters )
+    {
+        std::cout << c.size() << ", ";
+    }
+    std::cout << "}" << std::endl;
+
 	return clusters;
-
 }
 
 int main ()

c78e22e4e9152ca4dc63bb64702712254812dc81
Daniel Baudisch <daniel.baudisch@web.de>
2020-05-13 01:22:30 +0200

    udacity/detect_objects: initialized project from course
    YOLO data is quite a lot and was added as dedicated folder with a download
    script.

---
+++ Autonomous_Vehicles_and_Computer_Vision/yolo/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names

3a47756f7a71e8eeb0f9306e64bdb6b62be44ffe
Daniel Baudisch <daniel.baudisch@web.de>
2020-05-13 22:58:03 +0200

    udacity/SFND_2D_Feature_Tracking: apply toGrey conversion only for FLANN

--- SFND_2D_Feature_Tracking/src/matching2D_Student.cpp
+++ SFND_2D_Feature_Tracking/src/matching2D_Student.cpp
@@ -70,15 +70,6 @@ double matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::K
     cv::Ptr<cv::DescriptorMatcher> matcher;
 
 //         cerr << "CHECKPOINT: " << __FILE__ << ", " << __LINE__ << " in function " << __FUNCTION__ << endl;
-    if (descSource.type() != CV_32F)
-    { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
-        descSource.convertTo(descSource, CV_32F);
-    }
-    if (descRef.type() != CV_32F)
-    { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
-        descRef.convertTo(descRef, CV_32F);
-    }
-
     if (matcherType.compare("MAT_BF") == 0)
     {
         int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
@@ -87,6 +78,14 @@ double matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::K
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
+        if (descSource.type() != CV_32F)
+        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+            descSource.convertTo(descSource, CV_32F);
+        }
+        if (descRef.type() != CV_32F)
+        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+            descRef.convertTo(descRef, CV_32F);
+        }
         matcher = cv::DescriptorMatcher::create( cv::DescriptorMatcher::FLANNBASED );
         cout << "FLANN matching";
     }

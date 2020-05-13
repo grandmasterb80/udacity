53df5937c5eb3fab1e354bfd61552c5fa13329a4
Daniel Baudisch <daniel.baudisch@web.de>
2020-05-13 02:11:34 +0200

    udacity/yolo: updated download script (correct download from github)

--- Autonomous_Vehicles_and_Computer_Vision/yolo/download.sh
+++ Autonomous_Vehicles_and_Computer_Vision/yolo/download.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 wget https://pjreddie.com/media/files/yolov3.weights
-wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
-wget https://github.com/pjreddie/darknet/blob/master/data/coco.names
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
+
+git clone -n https://github.com/pjreddie/darknet.git --depth 1
+cd darknet/
+git checkout HEAD cfg/yolov3.cfg
+git checkout HEAD cfg/yolov3-tiny.cfg
+git checkout HEAD data/coco.names
+
+cd ..
+
+ln -s darknet/cfg/yolov3.cfg
+ln -s darknet/cfg/yolov3-tiny.cfg
+ln -s darknet/data/coco.names
+
+#wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
+#wget https://github.com/tahaemara/yolo-custom-object-detector/blob/afef9e2ab1fc91212a0206df3a06d23f4792e164/python/custom/yolov3-tiny.cfg
+

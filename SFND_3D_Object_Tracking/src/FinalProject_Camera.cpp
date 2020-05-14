adb09fd8cf4eb5cc65a4563ee5cb32b5e8bc9f2b
Daniel Baudisch <daniel.baudisch@web.de>
2020-05-14 16:23:12 +0200

    udacity/SFND_3D_Object_Tracking: project can now use newly added detectors

--- SFND_3D_Object_Tracking/src/FinalProject_Camera.cpp
+++ SFND_3D_Object_Tracking/src/FinalProject_Camera.cpp
@@ -157,9 +157,13 @@ int main(int argc, const char *argv[])
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
         }
+        else if (detectorType.compare("HARRIS") == 0)
+        {
+            detKeypointsHarris(keypoints, imgGray, false);
+        }
         else
         {
-            //...
+            detKeypointsModern( keypoints, imgGray, detectorType, false );
         }
 
         // optional : limit number of keypoints (helpful for debugging and learning)

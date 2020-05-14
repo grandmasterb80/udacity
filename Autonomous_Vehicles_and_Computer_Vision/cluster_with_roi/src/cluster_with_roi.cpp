a7ee6d6a2156c5993875507b429a3cd81d8d93cf
Daniel Baudisch <daniel.baudisch@web.de>
2020-05-14 02:21:05 +0200

    udacity/cluster_with_roi: added boundingbox / ROI visualization

--- Autonomous_Vehicles_and_Computer_Vision/cluster_with_roi/src/cluster_with_roi.cpp
+++ Autonomous_Vehicles_and_Computer_Vision/cluster_with_roi/src/cluster_with_roi.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
 void loadCalibrationData(cv::Mat &P_rect_00, cv::Mat &R_rect_00, cv::Mat &RT)
 {
     RT.at<double>(0,0) = 7.533745e-03; RT.at<double>(0,1) = -9.999714e-01; RT.at<double>(0,2) = -6.166020e-04; RT.at<double>(0,3) = -4.069766e-03;
@@ -26,6 +30,10 @@ void loadCalibrationData(cv::Mat &P_rect_00, cv::Mat &R_rect_00, cv::Mat &RT)
     P_rect_00.at<double>(2,0) = 0.000000e+00; P_rect_00.at<double>(2,1) = 0.000000e+00; P_rect_00.at<double>(2,2) = 1.000000e+00; P_rect_00.at<double>(2,3) = 0.000000e+00;
 
 }
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
 
 void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, cv::Size imageSize)
 {
@@ -63,11 +71,21 @@ void showLidarTopview(std::vector<LidarPoint> &lidarPoints, cv::Size worldSize, 
 
     // display image
     string windowName = "Top-View Perspective of LiDAR data";
-    cv::namedWindow(windowName, 2);
-    cv::imshow(windowName, topviewImg);
+//     cv::namedWindow(windowName, 2);
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+    {
+    //     cv::Mat imgSrc = cv::imread( filename_image );
+        cv::Mat visImg;
+        resize(topviewImg, visImg, cv::Size(), 0.25, 0.25, cv::INTER_CUBIC);
+        cv::imshow(windowName, visImg);
+//         cv::imshow(windowName, topviewImg);
+    }
     cv::waitKey(0); // wait for key to be pressed
 }
 
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
 
 void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<LidarPoint> &lidarPoints)
 {
@@ -120,6 +138,89 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes, std::vector<Li
     } // eof loop over all Lidar points
 }
 
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+void projectLidarToCamera2(cv::Mat &img, std::vector<LidarPoint> &lidarPoints, std::vector<BoundingBox> &boundingBoxes)
+{
+    // store calibration data in OpenCV matrices
+    cv::Mat P_rect_00(3,4,cv::DataType<double>::type); // 3x4 projection matrix after rectification
+    cv::Mat R_rect_00(4,4,cv::DataType<double>::type); // 4x4 rectifying rotation to make image planes co-planar
+    cv::Mat RT(4,4,cv::DataType<double>::type); // 4x4 rotation matrix and translation vector
+    loadCalibrationData(P_rect_00, R_rect_00, RT);
+    
+    // TODO: project lidar points
+    cv::Mat visImg = img.clone();
+    cv::Mat overlay = visImg.clone();
+
+    cv::Mat X(4,1,cv::DataType<double>::type);
+    cv::Mat Y(3,1,cv::DataType<double>::type);
+    for(auto it=lidarPoints.begin(); it!=lidarPoints.end(); ++it) {
+        float maxX = 25.0, maxY = 6.0, minZ = -1.4; 
+        if(it->x > maxX || it->x < 0.0 || abs(it->y) > maxY || it->z < minZ || it->r<0.01 )
+        {
+            continue; // skip to next point
+        }
+
+        // 1. Convert current Lidar point into homogeneous coordinates and store it in the 4D variable X.
+        X.at<double>(0,0) = it->x;
+        X.at<double>(1,0) = it->y;
+        X.at<double>(2,0) = it->z;
+        X.at<double>(3,0) = 1.0;
+
+        // 2. Then, apply the projection equation as detailed in lesson 5.1 to map X onto the image plane of the camera. 
+        // Store the result in Y.
+        Y = P_rect_00 * R_rect_00 * RT * X;
+
+        // 3. Once this is done, transform Y back into Euclidean coordinates and store the result in the variable pt.
+        cv::Point pt;
+        pt.x = Y.at<double>(0,0) / Y.at<double>(0,2);
+        pt.y = Y.at<double>(1,0) / Y.at<double>(0,2);
+
+        float val = it->x;
+        float maxVal = 20.0;
+        int red = min(255, (int)(255 * abs((val - maxVal) / maxVal)));
+        int green = min(255, (int)(255 * (1 - abs((val - maxVal) / maxVal))));
+        cv::circle(overlay, pt, 5, cv::Scalar(0, green, red), -1);
+    }
+
+    float opacity = 0.6;
+    cv::addWeighted(overlay, opacity, visImg, 1 - opacity, 0, visImg);
+
+    // ------------------------------------------------------------------------------------------------------------
+    // show results
+    for (auto it = boundingBoxes.begin(); it != boundingBoxes.end(); ++it)
+    {
+        // Draw rectangle displaying the bounding box
+        int top, left, width, height;
+        top = it->roi.y;
+        left = it->roi.x;
+        width = it->roi.width;
+        height = it->roi.height;
+        cv::rectangle(visImg, cv::Point(left, top), cv::Point(left + width, top + height), cv::Scalar(0, 255, 0), 2);
+
+//         string label = cv::format("%.2f", (*it).confidence);
+//         label = classes[((*it).classID)] + ":" + label;
+// 
+//         // Display label at the top of the bounding box
+//         int baseLine;
+//         cv::Size labelSize = getTextSize(label, cv::FONT_ITALIC, 0.5, 1, &baseLine);
+//         top = max(top, labelSize.height);
+//         rectangle(visImg, cv::Point(left, top - round(1.5 * labelSize.height)), cv::Point(left + round(1.5 * labelSize.width), top + baseLine), cv::Scalar(255, 255, 255), cv::FILLED);
+//         cv::putText(visImg, label, cv::Point(left, top), cv::FONT_ITALIC, 0.75, cv::Scalar(0, 0, 0), 1);
+    }
+    // ------------------------------------------------------------------------------------------------------------
+
+    string windowName = "LiDAR data on image overlay";
+    cv::namedWindow( windowName, 3 );
+    cv::imshow( windowName, visImg );
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
 int main()
 {
     std::vector<LidarPoint> lidarPoints;
@@ -127,6 +228,13 @@ int main()
 
     std::vector<BoundingBox> boundingBoxes;
     readBoundingBoxes("../dat/C53A3_currBoundingBoxes.dat", boundingBoxes);
+
+    // --------------------------------------------------------------------------------------
+    // load image from file
+//     ../../lidar_to_camera/images/0000000000.png
+    cv::Mat img = cv::imread( "../../OpenCV_exercises/images/img0005.png" );
+    projectLidarToCamera2( img, lidarPoints, boundingBoxes);
+   // --------------------------------------------------------------------------------------
 
     clusterLidarWithROI(boundingBoxes, lidarPoints);
     for (auto it = boundingBoxes.begin(); it != boundingBoxes.end(); ++it)

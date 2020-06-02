7a8edf35342ea8e44b54552f60d52cdc89cd3d26
Daniel Baudisch <daniel.baudisch@web.de>
2020-06-02 11:44:13 +0200

    udacity/SFND_Radar_Principles: added recourse "sensor-fusion-with-radar.m" for the project; added further smaller solutions from exercises

---
+++ SFND_Radar_Principles/Radar_Kalman_Tracking.m
@@ -0,0 +1,6 @@
+filter = trackingKF('MotionModel', model, 'State', state, 'MeasurementModel', measurementModel, 'StateCovariance', stateCovrariance, 'MeasurementNoise', measurementNoise)
+H = [1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]
+z  = H*x
+x = H'*z
+state = H'*detection.Measurement
+stateCovariance =H'*detection.MeasurementNoise*H

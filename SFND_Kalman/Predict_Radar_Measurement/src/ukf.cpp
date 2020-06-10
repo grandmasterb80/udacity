8bb1b67c14528a85d936abda3907389ddf2910a4
Daniel Baudisch <daniel.baudisch@web.de>
2020-06-10 18:27:15 +0200

    udacity/SFND_Kalman/Predict_Radar_Measurement: updated framework & added parts of solution

--- SFND_Kalman/Predict_Radar_Measurement/src/ukf.cpp
+++ SFND_Kalman/Predict_Radar_Measurement/src/ukf.cpp
@@ -16,12 +16,11 @@ void UKF::Init() {
 
 }
 
-
 /**
  * Programming assignment functions: 
  */
 
-void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+void UKF::PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out) {
 
   // set state dimension
   int n_x = 5;
@@ -29,105 +28,93 @@ void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
   // set augmented dimension
   int n_aug = 7;
 
-  // create example sigma point matrix
-  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
-  Xsig_aug <<
-    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
-      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
-    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
-    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
-    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
-         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
-         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+  // set measurement dimension, radar can measure r, phi, and r_dot
+  int n_z = 3;
 
-  // create matrix with predicted sigma points as columns
+  // define spreading parameter
+  double lambda = 3 - n_aug;
+
+  // set vector for weights
+  VectorXd weights = VectorXd(2*n_aug+1);
+  double weight_0 = lambda/(lambda+n_aug);
+  double weight = 0.5/(lambda+n_aug);
+  weights(0) = weight_0;
+
+  for (int i=1; i<2*n_aug+1; ++i) {  
+    weights(i) = weight;
+  }
+
+  // radar measurement noise standard deviation radius in m
+  double std_radr = 0.3;
+
+  // radar measurement noise standard deviation angle in rad
+  double std_radphi = 0.0175;
+
+  // radar measurement noise standard deviation radius change in m/s
+  double std_radrd = 0.1;
+
+  // create example matrix with predicted sigma points
   MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+  Xsig_pred <<
+         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
 
-  double delta_t = 0.1; // time diff in sec
+  // create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug + 1);
+
+  // mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  
+  // measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z,n_z);
 
   /**
    * Student part begin
    */
 
-  // predict sigma points
-  for( int i = 0; i < Xsig_pred.cols(); i++)
+  // transform sigma points into measurement space
+  
+
+  // calculate mean predicted measurement
+  z_pred.fill( 0.0 );
+  for( int i = 0; i < 2 * n_aug + 1; i++ )
   {
-    VectorXd x_k = Xsig_aug.block( 0, i, n_x, 1 );
-    double px            = x_k[ 0 ];
-    double py            = x_k[ 1 ];
-    double v             = x_k[ 2 ];
-    double psi           = x_k[ 3 ];
-    double psiD          = x_k[ 4 ];
-    double upsilon_a     = Xsig_aug( 5, i );
-    double upsilon_psiDD = Xsig_aug( 6, i );
-
-    // avoid division by zero
-    VectorXd p1 = VectorXd( n_x );
-    if( fabs( psiD ) <= 1.0e-10 ) // check for very small values instead for == 0.0
-    {
-        p1 << v * cos( psi ) * delta_t,
-              v * sin( psi ) * delta_t,
-              0.0,
-              psiD * delta_t,
-              0.0;
-    }
-    else
-    {
-        p1 << v / psiD * ( sin( psi + psiD * delta_t ) - sin( psi ) ),
-              v / psiD * (-cos( psi + psiD * delta_t ) + cos( psi ) ),
-              0.0,
-              psiD * delta_t,
-              0.0;
-    }
-    VectorXd p2 = VectorXd( n_x );
-    p2 << 0.5 * delta_t * delta_t * cos( psi ) * upsilon_a,
-          0.5 * delta_t * delta_t * sin( psi ) * upsilon_a,
-          delta_t * upsilon_a,
-          0.5 * delta_t * delta_t * upsilon_psiDD,
-          delta_t * upsilon_psiDD;
-    
-
-    VectorXd x_kp1 = x_k + p1 + p2;
-    //std::cout << "x_kp1 = " << std::endl << x_kp1 << std::endl;
-
-    // write predicted sigma points into right column
-    Xsig_pred.col(i) = Xsig_aug.block( 0, i, n_x, 1 ) + p1 + p2;
+      z_pred += weights( i ) * Zsig.col(i);
   }
+  
+  // calculate innovation covariance matrix S
+  S.fill( 0.0 );
+  for( int i = 0; i < 2 * n_aug + 1; i++ )
+  {
+      VectorXd t = Zsig.col(i) - z_pred;
+
+      // angle normalization (taken from lesson 4/24: Predicted mean and covariance solution)
+      // Reference: https://classroom.udacity.com/nanodegrees/nd313/parts/da5e72fc-972d-42ae-bb76-fca3d3b2db06/modules/c32eb575-0080-4a25-a087-98ab84cb3092/lessons/daf3dee8-7117-48e8-a27a-fc4769d2b954/concepts/07b59fdd-adb3-479b-8566-336332cf5f09
+      while (t(3)> M_PI) t(3)-=2.*M_PI;
+      while (t(3)<-M_PI) t(3)+=2.*M_PI;
+
+      S += weights( i ) * ( t * t.transpose().eval() );
+  }
+  
+  MatrixXd R( n_z, n_z );
+  R.fill( 0.0 );
+  R( 0, 0 ) = std_radr * std_radr;
+  R( 1, 1 ) = std_radphi * std_radphi;
+  R( 2, 2 ) = std_radrd * std_radrd;
+  S += R;
 
   /**
    * Student part end
    */
 
   // print result
-  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
-  
-  MatrixXd sollwert = MatrixXd( n_x, 2 * n_aug + 1);
-  sollwert <<
-    5.93553,  6.06251,   5.92217,   5.9415,    5.92361,   5.93516,   5.93705,  5.93553,   5.80832,   5.94481,   5.92935,   5.94553,   5.93589,   5.93401,  5.93553,
-    1.48939,  1.44673,   1.66484,   1.49719,   1.508,     1.49001,   1.49022,  1.48939,   1.5308,    1.31287,   1.48182,   1.46967,   1.48876,   1.48855,  1.48939,
-    2.2049,   2.28414,   2.24557,   2.29582,   2.2049,    2.2049,    2.23954,  2.2049,    2.12566,   2.16423,   2.11398,   2.2049,    2.2049,    2.17026,  2.2049,
-    0.53678,  0.473387,  0.678098,  0.554557,  0.643644,  0.543372,  0.53678,  0.538512,  0.600173,  0.395462,  0.519003,  0.429916,  0.530188,  0.53678,  0.535048,
-    0.3528,   0.299973,  0.462123,  0.376339,  0.48417,   0.418721,  0.3528,   0.387441,  0.405627,  0.243477,  0.329261,  0.22143,   0.286879,  0.3528,   0.318159;
-  std::cout << "Xsig_soll = " << std::endl << sollwert << std::endl;
-  std::cout << "Xsig_soll = " << std::endl << Xsig_pred - sollwert << std::endl;
-
+  std::cout << "z_pred: " << std::endl << z_pred << std::endl;
+  std::cout << "S: " << std::endl << S << std::endl;
 
   // write result
-  *Xsig_out = Xsig_pred;
+  *z_out = z_pred;
+  *S_out = S;
 }
-
-/*
-Expected output:
-  5.93553  6.06251   5.92217   5.9415    5.92361   5.93516   5.93705  5.93553   5.80832   5.94481   5.92935   5.94553   5.93589   5.93401  5.93553
-  1.48939  1.44673   1.66484   1.49719   1.508     1.49001   1.49022  1.48939   1.5308    1.31287   1.48182   1.46967   1.48876   1.48855  1.48939
-  2.2049   2.28414   2.24557   2.29582   2.2049    2.2049    2.23954  2.2049    2.12566   2.16423   2.11398   2.2049    2.2049    2.17026  2.2049
-  0.53678  0.473387  0.678098  0.554557  0.643644  0.543372  0.53678  0.538512  0.600173  0.395462  0.519003  0.429916  0.530188  0.53678  0.535048
-  0.3528   0.299973  0.462123  0.376339  0.48417   0.418721  0.3528   0.387441  0.405627  0.243477  0.329261  0.22143   0.286879  0.3528   0.318159
-*/
-/*
-  5.7441   5.85768   5.7441    5.7441    5.7441    5.7441    5.7441   5.7441  5.63052   5.7441    5.7441    5.7441    5.7441    5.7441  5.96071
-    1.38   1.34566   1.52806   1.38      1.38      1.38      1.38     1.38    1.41434   1.23194   1.38      1.38      1.38      1.38    1.50319
-  2.2049   2.28414   2.24557   2.29582   2.2049    2.2049    2.2049   2.2049  2.12566   2.16423   2.11398   2.2049    2.2049    2.2049  2.77931
-  0.5015   0.44339   0.631886  0.516923  0.595227  0.5015    0.5015   0.5015  0.55961   0.371114  0.486077  0.407773  0.5015    0.5015  0.5655
-  0.3528   0.299973  0.462123  0.376339  0.48417   0.418721  0.3528   0.3528  0.405627  0.243477  0.329261  0.22143   0.286879  0.3528  0.92721
-*/

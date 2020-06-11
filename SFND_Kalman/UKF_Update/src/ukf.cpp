bd99e55b2a8bdb746208209f531b774ed4a0f46b
Daniel Baudisch <daniel.baudisch@web.de>
2020-06-11 02:30:27 +0200

    udacity/SFND_Kalman/UKF_Update: added solution for assignment

--- SFND_Kalman/UKF_Update/src/ukf.cpp
+++ SFND_Kalman/UKF_Update/src/ukf.cpp
@@ -9,19 +9,17 @@ UKF::UKF() {
 }
 
 UKF::~UKF() {
-
 }
 
 void UKF::Init() {
 
 }
 
-
 /**
  * Programming assignment functions: 
  */
 
-void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+void UKF::UpdateState(VectorXd* x_out, MatrixXd* P_out) {
 
   // set state dimension
   int n_x = 5;
@@ -29,105 +27,142 @@ void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
   // set augmented dimension
   int n_aug = 7;
 
-  // create example sigma point matrix
-  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
-  Xsig_aug <<
-    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
-      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
-    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
-    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
-    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
-         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
-         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+  // set measurement dimension, radar can measure r, phi, and r_dot
+  int n_z = 3;
 
-  // create matrix with predicted sigma points as columns
+  // define spreading parameter
+  double lambda = 3 - n_aug;
+
+  // set vector for weights
+  VectorXd weights = VectorXd(2*n_aug+1);
+  double weight_0 = lambda/(lambda+n_aug);
+  double weight = 0.5/(lambda+n_aug);
+  weights(0) = weight_0;
+
+  for (int i=1; i<2*n_aug+1; ++i) {  
+    weights(i) = weight;
+  }
+
+  // create example matrix with predicted sigma points in state space
   MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+  Xsig_pred <<
+     5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+       1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+      2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+     0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+      0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
 
-  double delta_t = 0.1; // time diff in sec
+  // create example vector for predicted state mean
+  VectorXd x = VectorXd(n_x);
+  x <<
+     5.93637,
+     1.49035,
+     2.20528,
+    0.536853,
+    0.353577;
+
+  // create example matrix for predicted state covariance
+  MatrixXd P = MatrixXd(n_x,n_x);
+  P <<
+    0.0054342,  -0.002405,  0.0034157, -0.0034819, -0.00299378,
+    -0.002405,    0.01084,   0.001492,  0.0098018,  0.00791091,
+    0.0034157,   0.001492,  0.0058012, 0.00077863, 0.000792973,
+   -0.0034819,  0.0098018, 0.00077863,   0.011923,   0.0112491,
+   -0.0029937,  0.0079109, 0.00079297,   0.011249,   0.0126972;
+
+  // create example matrix with sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug + 1);
+  Zsig <<
+    6.1190,  6.2334,  6.1531,  6.1283,  6.1143,  6.1190,  6.1221,  6.1190,  6.0079,  6.0883,  6.1125,  6.1248,  6.1190,  6.1188,  6.12057,
+   0.24428,  0.2337, 0.27316, 0.24616, 0.24846, 0.24428, 0.24530, 0.24428, 0.25700, 0.21692, 0.24433, 0.24193, 0.24428, 0.24515, 0.245239,
+    2.1104,  2.2188,  2.0639,   2.187,  2.0341,  2.1061,  2.1450,  2.1092,  2.0016,   2.129,  2.0346,  2.1651,  2.1145,  2.0786,  2.11295;
+
+  // create example vector for mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  z_pred <<
+      6.12155,
+     0.245993,
+      2.10313;
+
+  // create example matrix for predicted measurement covariance
+  MatrixXd S = MatrixXd(n_z,n_z);
+  S <<
+      0.0946171, -0.000139448,   0.00407016,
+   -0.000139448,  0.000617548, -0.000770652,
+     0.00407016, -0.000770652,    0.0180917;
+
+  // create example vector for incoming radar measurement
+  VectorXd z = VectorXd(n_z);
+  z <<
+     5.9214,   // rho in m
+     0.2187,   // phi in rad
+     2.0062;   // rho_dot in m/s
+
+  // create matrix for cross correlation Tc
+  MatrixXd Tc = MatrixXd(n_x, n_z);
 
   /**
    * Student part begin
    */
 
-  // predict sigma points
-  for( int i = 0; i < Xsig_pred.cols(); i++)
+  // calculate cross correlation matrix
+  Tc.fill( 0.0 );
+  for( int i = 0; i < 2 * n_aug + 1; i++ )
   {
-    VectorXd x_k = Xsig_aug.block( 0, i, n_x, 1 );
-    double px            = x_k[ 0 ];
-    double py            = x_k[ 1 ];
-    double v             = x_k[ 2 ];
-    double psi           = x_k[ 3 ];
-    double psiD          = x_k[ 4 ];
-    double upsilon_a     = Xsig_aug( 5, i );
-    double upsilon_psiDD = Xsig_aug( 6, i );
+    VectorXd x_diff( n_x );
+    VectorXd z_diff( n_z );
+    x_diff = Xsig_pred.col( i ) - x;
+    z_diff = Zsig.col( i ) - z_pred;
+    // angle normalization (taken from the solution)
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+    // angle normalization (taken from the solution)
+    while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+    while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
 
-    // avoid division by zero
-    VectorXd p1 = VectorXd( n_x );
-    if( fabs( psiD ) <= 1.0e-10 ) // check for very small values instead for == 0.0
-    {
-        p1 << v * cos( psi ) * delta_t,
-              v * sin( psi ) * delta_t,
-              0.0,
-              psiD * delta_t,
-              0.0;
-    }
-    else
-    {
-        p1 << v / psiD * ( sin( psi + psiD * delta_t ) - sin( psi ) ),
-              v / psiD * (-cos( psi + psiD * delta_t ) + cos( psi ) ),
-              0.0,
-              psiD * delta_t,
-              0.0;
-    }
-    VectorXd p2 = VectorXd( n_x );
-    p2 << 0.5 * delta_t * delta_t * cos( psi ) * upsilon_a,
-          0.5 * delta_t * delta_t * sin( psi ) * upsilon_a,
-          delta_t * upsilon_a,
-          0.5 * delta_t * delta_t * upsilon_psiDD,
-          delta_t * upsilon_psiDD;
-    
-
-    VectorXd x_kp1 = x_k + p1 + p2;
-    //std::cout << "x_kp1 = " << std::endl << x_kp1 << std::endl;
-
-    // write predicted sigma points into right column
-    Xsig_pred.col(i) = Xsig_aug.block( 0, i, n_x, 1 ) + p1 + p2;
+    Tc += weights(i) * x_diff * z_diff.transpose().eval();
   }
+
+  // calculate Kalman gain K;
+  MatrixXd K( n_x, n_z );
+  K = Tc * S.inverse();
+
+  // update state mean and covariance matrix
+  VectorXd z_diff( n_z );
+  z_diff = z - z_pred;
+  // angle normalization (taken from the solution)
+  while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
+  while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
+
+  x += K * z_diff;
+  P -= K * (S * K.transpose());
+  
+  /// ---------------------------------------------------------------------------------
+  VectorXd x_soll( n_x );
+  x_soll << 5.92276, 1.41823, 2.15593, 0.489274, 0.321338;
+  MatrixXd P_soll( n_x, n_x );
+  P_soll << 0.00361579,  -0.000357881,  0.00208316,  -0.000937196, -0.00071727,
+           -0.000357881,  0.00539867,  0.00156846,  0.00455342,  0.00358885,
+            0.00208316,  0.00156846,  0.00410651,  0.00160333,  0.00171811,
+           -0.000937196,  0.00455342,  0.00160333,  0.00652634,  0.00669436,
+           -0.00071719,  0.00358884,  0.00171811,  0.00669426,  0.00881797;
+  /// ---------------------------------------------------------------------------------
 
   /**
    * Student part end
    */
 
   // print result
-  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
-  
-  MatrixXd sollwert = MatrixXd( n_x, 2 * n_aug + 1);
-  sollwert <<
-    5.93553,  6.06251,   5.92217,   5.9415,    5.92361,   5.93516,   5.93705,  5.93553,   5.80832,   5.94481,   5.92935,   5.94553,   5.93589,   5.93401,  5.93553,
-    1.48939,  1.44673,   1.66484,   1.49719,   1.508,     1.49001,   1.49022,  1.48939,   1.5308,    1.31287,   1.48182,   1.46967,   1.48876,   1.48855,  1.48939,
-    2.2049,   2.28414,   2.24557,   2.29582,   2.2049,    2.2049,    2.23954,  2.2049,    2.12566,   2.16423,   2.11398,   2.2049,    2.2049,    2.17026,  2.2049,
-    0.53678,  0.473387,  0.678098,  0.554557,  0.643644,  0.543372,  0.53678,  0.538512,  0.600173,  0.395462,  0.519003,  0.429916,  0.530188,  0.53678,  0.535048,
-    0.3528,   0.299973,  0.462123,  0.376339,  0.48417,   0.418721,  0.3528,   0.387441,  0.405627,  0.243477,  0.329261,  0.22143,   0.286879,  0.3528,   0.318159;
-  std::cout << "Xsig_soll = " << std::endl << sollwert << std::endl;
-  std::cout << "Xsig_soll = " << std::endl << Xsig_pred - sollwert << std::endl;
+  std::cout << "Updated state x: " << std::endl << x << std::endl;
+  std::cout << "Updated state covariance P: " << std::endl << P << std::endl;
 
+  std::cout << "x target: " << std::endl << x_soll << std::endl;
+  std::cout << "P target: " << std::endl << P_soll << std::endl;
+
+  std::cout << "x err: " << std::endl << (x_soll - x).array() / x_soll.array() << std::endl;
+  std::cout << "P err: " << std::endl << (P_soll - P).array() / P_soll.array() << std::endl;
 
   // write result
-  *Xsig_out = Xsig_pred;
+  *x_out = x;
+  *P_out = P;
 }
-
-/*
-Expected output:
-  5.93553  6.06251   5.92217   5.9415    5.92361   5.93516   5.93705  5.93553   5.80832   5.94481   5.92935   5.94553   5.93589   5.93401  5.93553
-  1.48939  1.44673   1.66484   1.49719   1.508     1.49001   1.49022  1.48939   1.5308    1.31287   1.48182   1.46967   1.48876   1.48855  1.48939
-  2.2049   2.28414   2.24557   2.29582   2.2049    2.2049    2.23954  2.2049    2.12566   2.16423   2.11398   2.2049    2.2049    2.17026  2.2049
-  0.53678  0.473387  0.678098  0.554557  0.643644  0.543372  0.53678  0.538512  0.600173  0.395462  0.519003  0.429916  0.530188  0.53678  0.535048
-  0.3528   0.299973  0.462123  0.376339  0.48417   0.418721  0.3528   0.387441  0.405627  0.243477  0.329261  0.22143   0.286879  0.3528   0.318159
-*/
-/*
-  5.7441   5.85768   5.7441    5.7441    5.7441    5.7441    5.7441   5.7441  5.63052   5.7441    5.7441    5.7441    5.7441    5.7441  5.96071
-    1.38   1.34566   1.52806   1.38      1.38      1.38      1.38     1.38    1.41434   1.23194   1.38      1.38      1.38      1.38    1.50319
-  2.2049   2.28414   2.24557   2.29582   2.2049    2.2049    2.2049   2.2049  2.12566   2.16423   2.11398   2.2049    2.2049    2.2049  2.77931
-  0.5015   0.44339   0.631886  0.516923  0.595227  0.5015    0.5015   0.5015  0.55961   0.371114  0.486077  0.407773  0.5015    0.5015  0.5655
-  0.3528   0.299973  0.462123  0.376339  0.48417   0.418721  0.3528   0.3528  0.405627  0.243477  0.329261  0.22143   0.286879  0.3528  0.92721
-*/

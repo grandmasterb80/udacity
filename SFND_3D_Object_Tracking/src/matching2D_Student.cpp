1162414eb3f3dbbf44635d326af38599fb8f9138
Daniel Baudisch <daniel.baudisch@web.de>
2020-10-09 00:36:12 +0200

    SFND_3D_Feature_Tracking: updated to OpenCV 4.4 (SIFT is now in main)

--- SFND_3D_Object_Tracking/src/matching2D_Student.cpp
+++ SFND_3D_Object_Tracking/src/matching2D_Student.cpp
@@ -108,7 +108,7 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
     }
     else if (descriptorType.compare("SIFT") == 0)
     {
-        extractor = cv::xfeatures2d::SIFT::create();
+        extractor = cv::SIFT::create();
     }
     else
     {
@@ -279,7 +279,7 @@ void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std:
     }
     else if( detectorType.compare("SIFT") == 0 )
     {
-        detector = cv::xfeatures2d::SIFT::create();
+        detector = cv::SIFT::create();
     }
 
 //     double t = (double)cv::getTickCount();
